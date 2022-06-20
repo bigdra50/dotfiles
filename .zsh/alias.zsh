fb011692adeda01c60e5c56d438f1f28847c8f29
bigdra50 <bigdra50@gmail.com>
2022-06-20 15:27:59 +0900

    :sparkles: add .zsh

---
+++ .zsh/alias.zsh
@@ -0,0 +1,32 @@
+# OS別の設定
+case ${OSTYPE} in
+  darwin*)
+    # Mac
+    alias ls="ls -FG"
+    ;;
+  linux*)
+    # Linux(wsl)
+    alias c='/mnt/c'
+    alias d='/mnt/d'
+    alias e='/mnt/e'
+    alias ls='LC_COLLATE=C ls --color=auto --human-readable --group-directories-first'
+    alias exp='explorer.exe'
+    alias open='cmd.exe /c start'
+    alias clip='clip.exe'
+    alias adb='adb.exe'
+    cd ~
+    ;;
+esac
+
+alias ll="ls -lh"
+alias la="ls -a"
+alias lal='ls -al'
+
+alias v='nvim'
+alias vz='nvim ~/.zshrc'
+alias vp='nvim ~/.zpreztorc'
+alias vv='nvim ~/.config/nvim/init.vim'
+alias h='history'
+alias so='source'
+alias soz='source ~/.zshenv && source ~/.zshrc'
+#alias mkdir='(){mkdir $1;cd $1}'
