9c628dc1b65ae16934a1740cfb124e05bc9e40f4
bigdra50 <bigdra50@gmail.com>
2022-10-10 13:19:02 +0900

    update

--- dot_zsh/alias.zsh
+++ dot_zsh/alias.zsh
@@ -1,14 +1,14 @@
 case ${OSTYPE} in
   darwin*)
     # Mac
-    alias ls="gls -FGS --group-directories-first"
+    #alias ls="gls -FGS --group-directories-first"
     ;;
   linux*)
     # Linux(wsl)
     alias c='/mnt/c'
     alias d='/mnt/d'
     alias e='/mnt/e'
-    alias ls='LC_COLLATE=C ls --color=auto --group-directories-first'
+    #alias ls='LC_COLLATE=C ls --color=auto --group-directories-first'
     alias exp='explorer.exe'
     alias open='cmd.exe /c start'
     alias clip='clip.exe'
@@ -17,26 +17,20 @@ case ${OSTYPE} in
     ;;
 esac
 
-alias ls="ls -F"
-alias ll="ls -lh"
-alias la="ls -a"
-alias lal='ls -al'
-
-alias rm='trash -rf'
 alias cp='cp -r'
 alias mkdir='mkdir -p'
+alias rm='trash -rf'
+alias cut='choose'
+alias df='duf'
+alias du='dust'
 
 alias g='git'
 
-
-if hascmd nvim; then
-  alias vim='nvim'
-fi
-
-alias v='vim'
-alias vz='vim ~/.zshrc'
-alias vv='vim ~/.config/nvim/init.vim'
+DOTFILES=$XDG_DATA/chezmoi
+alias v='nvim'
+alias vz='vim $DOTFILES/dot_zshrc'
+alias vv='vim $DOTFILES/dot_config/nvim/init.vim'
 alias h='history'
 alias so='source'
-alias soz='source ~/.zshenv && source ~/.zshrc'
+alias soz='source $HOME/.zshenv && source $HOME/.zshrc'
 #alias mkdir='(){mkdir $1;cd $1}'
