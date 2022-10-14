3f18f8b1601ea6182610897e0ee70a5ed7d7b7cd
bigdra <bigdra50@gmail.com>
2022-10-14 20:27:16 +0900

    update zsh config

--- dot_zsh/history.zsh
+++ dot_zsh/history.zsh
@@ -5,12 +5,19 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=100000
+# 重複するコマンドなら､古いものを削除
 setopt hist_ignore_all_dups
+# 重複を記録しない
 setopt hist_ignore_dups
 setopt share_history
 setopt append_history
+# 保管時に履歴を自動的に展開
+setopt hist_expand
+# 履歴をインクリメンタルに追加
 setopt inc_append_history
+# historyコマンドは履歴に登録しない
 setopt hist_no_store
+# 余分な空白は削除
 setopt hist_reduce_blanks
 zstyle ':completion:*:default' menu select
 
@@ -22,3 +29,5 @@ zle -N history-beginning-search-forward-end history-search-end
 bindkey "^P" history-beginning-search-backward-end
 bindkey "^N" history-beginning-search-forward-end
 bindkey "^R" history-incremental-search-backward
+
+function history-all { history -E 1 }
