7e52a584fb88f0c5af240129f5c66f21df8a06da
bigdra <bigdra50@gmail.com>
2022-12-26 11:04:00 +0900

    update

--- zsh.nix
+++ zsh.nix
@@ -5,10 +5,28 @@
   programs.zsh = {
     enable = true;
     autocd = true;
-    # dotDir = ".config/zsh";
-
+    dotDir = ".config/zsh";
     enableCompletion = true;
     enableAutosuggestions = true;
     enableSyntaxHighlighting = true;
+    shellAliases = {
+      cp = "cp -r";
+      mkdir = "mkdir -p";
+      rm = "trash -rf";
+      cut = "choose";
+      df = "duf";
+      du = "dust";
+      restart = "exec $SHELL -l";
+      top = "btm";
+      diff = "delta";
+      sl = "lsd";
+      ls = "lsd";
+      l = "lsd -l";
+      la = "lsd -la";
+      ip = "ip --color=auto";
+      g = "git";
+      v = "nvim";
+      h = "history";
+    };
   };
 }
