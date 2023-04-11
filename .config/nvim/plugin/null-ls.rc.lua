49304f1c5f3e60dfe244b8c96e1c89d40ad7aa04
bigdra50 <bigdra50@gmail.com>
2023-04-11 20:25:46 +0900

    Add eslint_d and prettierd as sources for null-ls and map <Space>fmt to format the buffer using LSP in null-ls.rc.lua

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -4,14 +4,16 @@ if (not status) then return end
 null_ls.setup({
     -- add your sources / config options here
     sources = {
-        null_ls.builtins.diagnostics.eslint.with({
-        prefer_local = "node_modules/.bin",
-    }),
-        null_ls.builtins.formatting.prettier,
-        -- null_ls.builtins.diagnostics.rubocop,
-        -- null_ls.builtins.formatting.rubocop,
+      null_ls.builtins.diagnostics.eslint_d.with({
+      prefer_local = "node_modules/.bin",
+      }),
+      null_ls.builtins.formatting.prettierd.with({
+      prefer_local = "node_modules/.bin",
+      }),
+      -- null_ls.builtins.diagnostics.rubocop,
+      -- null_ls.builtins.formatting.rubocop,
     },
     debug = false,
 })
 -- formatを使う
-vim.keymap.set('n', '<leader>mm', function() vim.lsp.buf.format { async = true } end)
+vim.keymap.set('n', '<Space>fmt', function() vim.lsp.buf.format { async = true } end)
