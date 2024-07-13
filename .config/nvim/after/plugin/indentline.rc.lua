54869f1b8472d6e1ba69ed654c3ea5c4302b835d
bigdra <bigdra50@gmail.com>
2024-07-13 13:00:57 +0900

    :sparkles: Update nvim

--- .config/nvim/after/plugin/indentline.rc.lua
+++ .config/nvim/after/plugin/indentline.rc.lua
@@ -1,3 +1,6 @@
+--require("ibl").setup {
+--  scope = { enabled = false }
+--}
 local highlight = {
     "RainbowRed",
     "RainbowYellow",
@@ -7,9 +10,8 @@ local highlight = {
     "RainbowViolet",
     "RainbowCyan",
 }
+
 local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -20,7 +22,13 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-vim.g.rainbow_delimiters = { highlight = highlight }
-require("ibl").setup { scope = { highlight = highlight } }
+require("ibl").setup {
+    scope = {
+        highlight = highlight,
+    },
+    indent = {
+        highlight = highlight,
+    },
+}
 
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
