d8d5bea8092e5842e392057a36c6ba65350596b0
bigdra50 <bigdra50@gmail.com>
2024-07-15 12:06:09 +0900

    :wrench: Update

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -1,29 +1,44 @@
+-- nvim-treesitterの設定
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
-require 'nvim-treesitter.install'.compilers = { "/usr/bin/gcc" }
-treesitter.setup {
-  ensure_installed = {"vim", "dockerfile", "fish", "typescript", "tsx", "javascript", "json", "lua", "gitignore", "bash", "astro", "markdown", "markdown_inline", "css", "scss", "yaml", "toml", "vue", "php", "html"},
-  -- ignore_install = { "html" },
+treesitter.setup({
+  incremental_selection = {
+    enable = false,
+    keymaps = {
+      scope_incremental = "a",
+      node_decremental = "z",
+    },
+  },
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = true, -- catpuucin用
-    disable = {"help"},
   },
-  indent = {
-    enable = true,
+  indent = { enable = true },
+  autotag = { enable = true },
+  ensure_installed = {
+    "json",
+    "yaml",
+    "markdown",
+    "markdown_inline",
+    "lua",
+    "gitignore",
+    "swift",
   },
-}
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "help",
-  callback = function()
-    vim.bo.syntax = "ON"
-    vim.bo.filetype = "help"
-    -- Tree-sitter を無効にする
-    vim.cmd([[TSBufDisable highlight]])
-    vim.bo.syntax = "ON"
-  end,
+  auto_install = true,
 })
 
-require('nvim-ts-autotag').setup()
+-- nvim-ts-autotagの設定
+local autotag_status, autotag = pcall(require, "nvim-ts-autotag")
+if autotag_status then
+  autotag.setup()
+end
+
+-- TSUpdateコマンドの実行（ビルド相当）
+vim.cmd([[TSUpdate]])
+
+-- イベントに基づいてTreesitterを読み込む
+vim.api.nvim_create_autocmd({"BufReadPre", "BufNewFile"}, {
+  callback = function()
+    -- ここに必要な追加の設定やコマンドを記述できます
+  end,
+})
