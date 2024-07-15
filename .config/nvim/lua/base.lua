d721d54fbeb49e16942011d66b543d2d4023f55e
bigdra <bigdra50@gmail.com>
2024-07-15 17:47:05 +0900

    :wrench: Updatge

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -1,11 +1,11 @@
 vim.cmd("autocmd!")
 
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 vim.opt.compatible = false
 vim.opt.number = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
@@ -17,38 +17,37 @@ vim.opt.cmdheight = 2
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
 --vim.opt.shell = 'zsh'
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 vim.opt.smarttab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.wrap = false
-vim.opt.helplang = {'ja', 'en'}
+vim.opt.helplang = { "ja", "en" }
 vim.opt.updatetime = 300
 vim.opt.showtabline = 2
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 vim.opt.winblend = 0
 vim.opt.pumblend = 0
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.hidden = true
 vim.opt.swapfile = false
 vim.opt.pumblend = 7
 vim.wo.relativenumber = true
-vim.opt.conceallevel = 1
 
 -- 不可視文字を非表示(colorscheme用)
 vim.opt.list = false
 
 -- Leder Key
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
 -- キーバインド
 -- 画面分割
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+keymap.set("n", "ss", ":split<Return><C-w>w")
+keymap.set("n", "sv", ":vsplit<Return><C-w>w")
 -- アクティブウィンドウの移動
 --keymap.set('', 'sh', '<C-w>h')
 --keymap.set('', 'sk', '<C-w>k')
@@ -56,10 +55,10 @@ keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 --keymap.set('', 'sl', '<C-w>l')
 
 -- Emacs like keybinding
-keymap.set('i', '<C-f>', '<Right>')
+keymap.set("i", "<C-f>", "<Right>")
 
 -- jjでEscする
-keymap.set('i','jj','<Esc>')
+keymap.set("i", "jj", "<Esc>")
 
 -- 設定ファイルを開く
-keymap.set('n','<F1>',':edit $MYVIMRC<CR>')
+keymap.set("n", "<F1>", ":edit $MYVIMRC<CR>")
